56b6c68193745bcc4646e4d529a7c2627bda95c1
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-03-19 21:49:53 +0300

    The assignment is done

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+       let num1 = 31;
+       let num2 = 2;
+       let multiply = num1 * num2 ;
+       num1 = 31;
+       num2 = 2; 
+       console.log(multiply);
+
+       let random = Math.floor(Math.random() * 100) + 1;
+       console.log(random);
+
+       let num3 = 14;
+       let num4 = 5;
+       let mod = num3 % num4;
+       console.log(mod);
+
+       let set = [5, 12, 20, 8, 3];
+       let max = Math.max(...set);
+       console.log(max);
